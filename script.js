aed765f89c96efa2c629443f21a80da9b9597da4
Nancy727 <nancykumari742004@gmail.com>
2025-06-26 19:23:55 +0530

    update chnages

--- script.js
+++ script.js
@@ -12,20 +12,33 @@ function Loading(){
     duration:0.5,
     ease:"expo.out"
     },"anim")
-    tl.to("#loader h1",{
-    delay:0.8,
-    delay:0.5,
-    color:"black"
-    },"anim")
-    tl.to("#loader",{
-    display:"none"
-    })
+   tl.to("#loader h1", {
+  delay: 0.5,
+  color: "black"
+}, "anim")
+
+   tl.to("#loader", {
+  opacity: 0,
+  duration: 0.5,
+  onComplete: () => {
+    document.querySelector("#loader").style.display = "none";
+  }
+});
+
     tl.to("#loader",{
     opacity:0
     })
 }
 Loading()
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
+var elems=document.querySelectorAll(".elem");
+var page2=document.querySelector("#page2")
+elems.forEach(function(ele){
+      ele.addEventListener("mouseenter",function(){
+      var bgimg=ele.getAttribute("data-img")
+      page2.style.backgroundImage=`url(${bgimg})`
+      })
+})
